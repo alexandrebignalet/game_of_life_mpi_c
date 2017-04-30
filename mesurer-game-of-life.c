7b9fceb3f72d91c4c4e2bd3d372913133da6d193
alexandrebignalet <alexandre.bignalet@gmail.com>
2017-04-30 13:46:24 -0400

    adding mpi config

--- mesurer-game-of-life.c
+++ mesurer-game-of-life.c
@@ -2,10 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <mpi.h>
 
 int main(int argc, char *argv[])
 {
+    int ID, nbProcs;
+
+    // On initialise MPI.
+    MPI_Init( &argc, &argv );
+    MPI_Comm_rank( MPI_COMM_WORLD, &ID );
+    MPI_Comm_size( MPI_COMM_WORLD, &nbProcs );
+
     srand(time(NULL));
 
     int SIZE_WITH_GHOSTS_CELLS = GENERATION_SIZE + GHOST_CELLS_SIZE; // New array size including ghost cells size
@@ -14,5 +21,6 @@ int main(int argc, char *argv[])
 
     printf("\nNumber of live cells = %d\n", nb_live_cells);
 
+    MPI_Finalize();
     return 0;
 }
