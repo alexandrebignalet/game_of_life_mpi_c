dc7b805508182fa9c80fa3854ae63ab840fc6542
alexandrebignalet <alexandre.bignalet@gmail.com>
2017-05-02 20:07:20 -0400

    adding source

--- game_of_life.c
+++ game_of_life.c
@@ -8,6 +8,99 @@ sequential version
 
 #include "game_of_life.h"
 
+int game_of_life_seq(int size, int nb_steps, int repartition_probability) {
+
+    int width = size + GHOST_CELLS_SIZE;
+    int *matrix = (int *) malloc(size * width * sizeof(int));
+
+    initialize(matrix, size, width);
+
+    //set first and last lines as ghosts cells
+    int* first_line = &(matrix[0]);
+    int* last_line = &(matrix[(size-1)*width]);
+    for(int i = 0; i < width; i++) {
+        *first_line++ = DEAD;
+        *last_line++ = DEAD;
+    }
+
+    for(int step = 0; step < nb_steps; step++) {
+        generation(matrix, size, width);
+    }
+
+    int nb_cells_alive = 0;
+    for (int j = 0; j < size*width; j++) {
+        nb_cells_alive += *matrix++;
+    }
+
+    return nb_cells_alive;
+}
+
+void initialize(int* matrix, int size, int repartition_probability) {
+    int* pt = matrix;
+    int rand_number;
+    float predicate;
+
+    for ( int i = 0; i < size; i++ ) {
+        *pt++ = DEAD;
+
+        for ( int j = 0; j < size; j++ ) {
+            rand_number = rand() % repartition_probability;
+            predicate = (float) 1 / repartition_probability;
+
+            if( rand_number < predicate )
+            {
+                *pt++ = ALIVE;
+            } else {
+                *pt++ = DEAD;
+            }
+        }
+
+        *pt++ = DEAD;
+    }
+}
+void generation(int* block, int block_size, int size) {
+
+    int *ptr = &(block[size+1]);
+
+    for( int x = 1; x < block_size-1; x++){
+
+        for( int y = 1; y < size-1; y++){
+
+            switch(sumCellNeighborhoodState(block, size, size*x+y)) {
+
+                case CELL_GOES_ALIVE:
+                    *ptr++ = ALIVE;
+                    break;
+
+                case CELL_KEEP_STATE:
+                    *ptr++;
+                    break;
+
+                default:
+                    *ptr++ = DEAD;
+                    break;
+            }
+        }
+        *ptr++;
+        *ptr++;
+    }
+}
+int sumCellNeighborhoodState(int* matrix, int size, int index) {
+
+    return matrix[index - size + 1] + matrix[index - size] + matrix[index - size - 1]
+           + matrix[index - 1] + matrix[index + 1]
+           + matrix[index + size - 1] + matrix[index + size] + matrix[index + size + 1];
+}
+
+// by line
+void updateLines(int* lines, int line_size, int* line, int index) {
+    int* ptr_lines = &(lines[index]);
+    int* ptr_line = line;
+
+    for(int i = 0 ; i < line_size; i++) {
+        *ptr_lines++ = *ptr_line++;
+    }
+}
 int game_of_life_by_line(int size, int nb_steps, int repartition_probability) {
 
 
@@ -29,8 +122,6 @@ int game_of_life_by_line(int size, int nb_steps, int repartition_probability) {
     int last = nb_procs - 1;
 
     if (my_id == ROOT) {
-
-        int begin, end;
 
         initialize(matrix, height, repartition_probability);
 
@@ -85,108 +176,6 @@ int game_of_life_by_line(int size, int nb_steps, int repartition_probability) {
 
     return nb_cells_alive;
 }
-
-int game_of_life_seq(int size, int nb_steps, int repartition_probability) {
-
-    int width = size + GHOST_CELLS_SIZE;
-    int *matrix = (int *) malloc(size * width * sizeof(int));
-
-    initialize(matrix, size, width);
-
-    //set first and last lines as ghosts cells
-    int* first_line = &(matrix[0]);
-    int* last_line = &(matrix[(size-1)*width]);
-    for(int i = 0; i < width; i++) {
-        *first_line++ = DEAD;
-        *last_line++ = DEAD;
-    }
-
-    for(int step = 0; step < nb_steps; step++) {
-        generation(matrix, size, width);
-    }
-
-    int nb_cells_alive = 0;
-    for (int j = 0; j < size*width; j++) {
-        nb_cells_alive += *matrix++;
-    }
-
-    return nb_cells_alive;
-}
-
-void updateLines(int* lines, int line_size, int* line, int index) {
-    int* ptr_lines = &(lines[index]);
-    int* ptr_line = line;
-
-    for(int i = 0 ; i < line_size; i++) {
-        *ptr_lines++ = *ptr_line++;
-    }
-}
-
-void initialize(int* matrix, int size, int repartition_probability) {
-    int* pt = matrix;
-    int rand_number;
-    float predicate;
-
-    for ( int i = 0; i < size; i++ ) {
-        *pt++ = DEAD;
-
-        for ( int j = 0; j < size; j++ ) {
-            rand_number = rand() % repartition_probability;
-            predicate = (float) 1 / repartition_probability;
-
-            if( rand_number < predicate )
-            {
-                *pt++ = ALIVE;
-            } else {
-                *pt++ = DEAD;
-            }
-        }
-
-        *pt++ = DEAD;
-    }
-}
-
-int sumCellNeighborhoodState(int* matrix, int size, int index) {
-
-    return matrix[index - size + 1] + matrix[index - size] + matrix[index - size - 1]
-           + matrix[index - 1] + matrix[index + 1]
-           + matrix[index + size - 1] + matrix[index + size] + matrix[index + size + 1];
-}
-
-void printLine(int *matrix, int size_x, int size_y) {
-    int* ptr = matrix;
-
-    for (int i = 0; i < size_x; i++) {
-        printf("\n");
-        for (int j = 0; j < size_y; j++) {
-            printf("%d ", *ptr++);
-        }
-    }
-}
-
-void printRange(int* matrix, int begin, int size_x, int size_y) {
-    int* ptr = &(matrix[begin]);
-
-    for (int i = 0; i < size_x; i++) {
-        printf("\n");
-        for (int j = 0; j < size_y; j++) {
-            printf("%d ", *ptr++);
-        }
-    }
-}
-
-void print(int *matrix, int width, int height) {
-
-    int* pt = matrix;
-
-    for ( int i = 0; i < width; i++ ) {
-        printf("\n");
-        for (int j = 0; j < height; j++) {
-            printf("%d ", *pt++);
-        }
-    }
-}
-
 void getLines(int* matrix, int size, int* buffer, int block_size, int num_proc) {
 
     int nb_procs;
@@ -231,30 +220,70 @@ void getLines(int* matrix, int size, int* buffer, int block_size, int num_proc) 
     }
 }
 
-void generation(int* line_block, int block_size, int size) {
+// by block
+int game_of_life_by_block(int size, int nb_steps, int repartition_probability) {
+    int my_id, nb_procs;
 
-    int *ptr = &(line_block[size+1]);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+    MPI_Comm_size(MPI_COMM_WORLD, &nb_procs);
 
-    for( int x = 1; x < block_size-1; x++){
+    int nb_cells_alive = 0;
+    int nb_blocs = sqrt(nb_procs);
+    int block_size = size / sqrt(nb_procs);
+    int block_size_with_halo = block_size + GHOST_CELLS_SIZE;
 
-        for( int y = 1; y < size-1; y++){
+    int height = size;
+    int width = size + GHOST_CELLS_SIZE;
 
-            switch(sumCellNeighborhoodState(line_block, size, size*x+y)) {
+    int *matrix = (int *) malloc(height * width * sizeof(int));
+    int *my_block = (int *) malloc( block_size * block_size * sizeof(int));
 
-                case CELL_GOES_ALIVE:
-                    *ptr++ = ALIVE;
-                    break;
+    if (my_id == ROOT) {
 
-                case CELL_KEEP_STATE:
-                    *ptr++;
-                    break;
+        initialize(matrix, size, repartition_probability);
 
-                default:
-                    *ptr++ = DEAD;
-                    break;
-            }
+        for(int num_proc = 1; num_proc < nb_procs; num_proc++) {
+            getBlock(matrix, size, my_block, block_size, num_proc);
+            MPI_Send(my_block, block_size_with_halo * block_size_with_halo, MPI_INT, num_proc, 0, MPI_COMM_WORLD);
         }
-        *ptr++;
-        *ptr++;
+        getBlock(matrix, size, my_block, block_size, ROOT);
+    } else {
+        MPI_Recv(my_block, block_size_with_halo * block_size_with_halo, MPI_INT, ROOT, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+
+    return nb_cells_alive;
+}
+
+// utils
+void printLine(int *matrix, int size_x, int size_y) {
+    int* ptr = matrix;
+
+    for (int i = 0; i < size_x; i++) {
+        printf("\n");
+        for (int j = 0; j < size_y; j++) {
+            printf("%d ", *ptr++);
+        }
+    }
+}
+void printRange(int* matrix, int begin, int size_x, int size_y) {
+    int* ptr = &(matrix[begin]);
+
+    for (int i = 0; i < size_x; i++) {
+        printf("\n");
+        for (int j = 0; j < size_y; j++) {
+            printf("%d ", *ptr++);
+        }
+    }
+}
+void print(int *matrix, int width, int height) {
+
+    int* pt = matrix;
+
+    for ( int i = 0; i < width; i++ ) {
+        printf("\n");
+        for (int j = 0; j < height; j++) {
+            printf("%d ", *pt++);
+        }
     }
 }
--- game_of_life.h
+++ game_of_life.h
@@ -17,20 +17,19 @@
 #define ROOT 0
 
 
-int game_of_life_by_line(int size, int nb_steps, int repartition_probability);
-
 int game_of_life_seq(int size, int nb_steps, int repartition_probability);
 
 void initialize(int* matrix, int size, int repartition_probability);
-
-void getLines(int* matrix, int size, int* buffer, int block_size, int num_proc);
-
-void updateLines(int* lines, int line_size, int* line, int index);
-
-void generation(int* line_block, int block_size, int size);
-
+void generation(int* block, int block_size, int size);
 int sumCellNeighborhoodState(int* matrix, int size, int index);
 
+// by line
+int game_of_life_by_line(int size, int nb_steps, int repartition_probability);
+void getLines(int* matrix, int size, int* buffer, int block_size, int num_proc);
+void updateLines(int* lines, int line_size, int* line, int index);
+
+// by block
+void getBlock(int* matrix, int size, int* buffer, int block_size, int num_proc);
 
 
 void print(int *matrix, int width, int height);
